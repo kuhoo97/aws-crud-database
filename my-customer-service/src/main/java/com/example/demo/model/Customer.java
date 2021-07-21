698df5a716f5d6f91bbb47f0cc42258aec514c0d
pooja yadav <kuhoo97@gmail.com>
2021-07-21 17:09:41 +0530

    updated

--- my-customer-service/src/main/java/com/example/demo/model/Customer.java
+++ my-customer-service/src/main/java/com/example/demo/model/Customer.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class Customer {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	@Column(name = "customer_name")
 	private String customerName;
