9f17255595f7d0dd6430328ffaa2db0b19149e4d
pooja yadav <kuhoo97@gmail.com>
2021-07-21 16:27:26 +0530

    customer-aws-database-muSql

---
+++ my-customer-service/src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Column(name = "customer_name")
+	private String customerName;
+	@Column(name = "customer_address")
+	private String customerAddress;
+	
+
+}
